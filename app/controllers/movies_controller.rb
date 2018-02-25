1d604e6315ad5d69c6b8ceb836697ec5a2e7290f
grturner <grturner@5x5code.com>
2018-02-24 19:37:07 -0500

    Section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,15 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(id)
 	end
 
+	def new
+		@movie = Movie.new
+		# default: render 'new' template
+	end
+
 	# add below all other methods
 	private
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
+	def movie_params
+		params.require(:movie).permit(:title, :rating, :description, :release_date)
+	end
 
 end
